7bbe71c854f67804846e38b5b811f51c8c338000
mi nombre <desarrollowebaps@gmail.com>
2023-01-05 13:31:58 -0400

    11_template_fashion

--- js/action.js
+++ js/action.js
@@ -1,5 +1,12 @@
 let action = document.querySelector("#action")
-//let ul_action = document.querySelector(".ul_action")
+let ul_action = document.querySelector("#ul_action")
+
+
+
+action.addEventListener("mouseover", ()=>{
+  ul_action.classList.toggle("ul_actionJS")
+})
+
 
 
 /*
