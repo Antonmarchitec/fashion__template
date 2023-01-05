f5c008d86a8f64bd8aa37d9c03cf11b1bb1c9e74
mi nombre <desarrollowebaps@gmail.com>
2023-01-05 01:04:15 -0400

    10_template_fashion

--- js/action.js
+++ js/action.js
@@ -1,6 +1,20 @@
 let action = document.querySelector("#action")
-let ul_action = document.querySelector(".ul_action")
+//let ul_action = document.querySelector(".ul_action")
 
-action.addEventListener("mouseover", ()=>{
-    ul_action.classList.toggle("ul_action_JS")
-})
+
+/*
+<h1 id="demo">Mouse over me</h1>
+
+<script>
+document.getElementById("demo").addEventListener("mouseover", mouseOver);
+document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.color = "black";
+}
+</script>
+*/
