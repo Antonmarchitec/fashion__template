9cbdab693001687d7d016fa2cdd819a6689c608c
mi nombre <desarrollowebaps@gmail.com>
2023-01-28 18:43:25 -0400

    16_template_fashion

--- js/swiper.js
+++ js/swiper.js
@@ -2,9 +2,9 @@
 
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
@@ -18,23 +18,23 @@ var swiper = new Swiper(".mySwiper", {
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20,
+        spaceBetween: 10,
       },
-      423: {
+      424: {
         slidesPerView: 2,
-        spaceBetween: 20,
+        spaceBetween: 10,
       },
       680: {
         slidesPerView: 3,
-        spaceBetween: 40,
+        spaceBetween: 10,
       },
       920: {
         slidesPerView: 4,
-        spaceBetween: 20,
+        spaceBetween: 10,
       },
       1240: {
         slidesPerView: 4,
-        spaceBetween: 20,
+        spaceBetween: 10,
       },
     } 
   });
